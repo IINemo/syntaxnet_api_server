74858633706eda23f1f0c8c1f5259cbf805e8c84
artem <artem@localhost.localdomain>
2017-02-07 12:26:29 +0300

    q

--- syntaxnext_rus_api.py
+++ syntaxnext_rus_api.py
@@ -27,6 +27,25 @@ sys.path += python_path_entries
 
 ################################################################################
 
+import logging
+
+
+def configure_logger():
+    core_logger = logging.getLogger('common_logger')
+    core_logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stderr)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - '
+                                  '%(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    core_logger.addHandler(ch)
+    return core_logger
+
+
+logger = configure_logger()
+
+################################################################################
+
 import shutil
 
 custom_file_path = '/root/models/syntaxnet/bazel-bin/syntaxnet/parser_eval.runfiles/__main__/syntaxnet/api/context.pbtxt'
@@ -49,6 +68,7 @@ from syntaxnet.ops import gen_parser_ops
 from syntaxnet import task_spec_pb2
 
 import StringIO
+import select
 
 
 beam_size = 8
@@ -68,9 +88,6 @@ input_str = 'custom_file'
 output_str = 'stdout-conll'
 batch_size = 1024
 hidden_layer_str = '64'
-
-# fifo_path = '/tmp/syntax_net_testpipe'
-# os.system('mkfifo' + ' ' + fifo_path)
 
 def RewriteContext(task_context_file):
   context = task_spec_pb2.TaskSpec()
@@ -109,8 +126,6 @@ def init(sess, task_context):
                        batch_size,
                        corpus_name=input_str,
                        evaluation_max_steps=max_steps)
-  print parser.evaluation.keys()
-  sys.stdout.flush()
   
   parser.AddSaver(slim_model)
   sess.run(parser.inits.values())
@@ -119,24 +134,71 @@ def init(sess, task_context):
   return parser
   
 
-def parse(parser, task_context):
-  tf_eval_epochs, tf_eval_metrics, tf_documents = sess.run([
-        parser.evaluation['epochs'],
-        parser.evaluation['eval_metrics'],
-        parser.evaluation['documents']
-   ])
 
-  sink_documents = tf.placeholder(tf.string)
-  sink = gen_parser_ops.document_sink(sink_documents,
-                                      task_context=task_context,
-                                      corpus_name=output_str)
 
-  sess.run(sink, feed_dict={sink_documents: tf_documents})
+
+class ProcessorSyntaxNet(object):
+  def __init__(self):
+    super(ProcessorSyntaxNet, self).__init__()
+    hidden_layer_sizes = map(int, hidden_layer_str.split(','))
+
+    feature_sizes, domain_sizes, embedding_dims, num_actions = sess.run(
+      gen_parser_ops.feature_size(task_context=task_context,
+        arg_prefix=arg_prefix))
+
+    parser = structured_graph_builder.StructuredGraphBuilder(
+            num_actions,
+            feature_sizes,
+            domain_sizes,
+            embedding_dims,
+            hidden_layer_sizes,
+            gate_gradients=True,
+            arg_prefix=arg_prefix,
+            beam_size=beam_size,
+            max_steps=max_steps)
+
+    parser.AddEvaluation(task_context,
+                         batch_size,
+                         corpus_name=input_str,
+                         evaluation_max_steps=max_steps)
+    
+    parser.AddSaver(slim_model)
+    sess.run(parser.inits.values())
+    parser.saver.restore(sess, model_path)
+    
+    return parser
+
+  def parse(self, raw_bytes):
+    with open(custom_file_path, 'w') as f:
+      pass 
+
+    self._parse_impl(self.server.parser_, self.server.task_context_)
+    sys.stdout.flush()
+    
+    with open(custom_file_path, 'a') as f:
+      f.write('\n' + data)
+      f.flush()
+
+    self._parse_impl(self.server.parser_, self.server.task_context_)
+    sys.stdout.flush()
+
+  def _parse_iml(self, parser, task_context):
+    tf_eval_epochs, tf_eval_metrics, tf_documents = sess.run([
+          parser.evaluation['epochs'],
+          parser.evaluation['eval_metrics'],
+          parser.evaluation['documents']
+     ])
+
+    sink_documents = tf.placeholder(tf.string)
+    sink = gen_parser_ops.document_sink(sink_documents,
+                                        task_context=task_context,
+                                        corpus_name=output_str)
+
+    sess.run(sink, feed_dict={sink_documents: tf_documents})
 
 
 class SyncHandler(SocketServer.BaseRequestHandler):
-  def handle(self):
-    sys.stdout.flush()
+  def read_incoming_request(self):
     chunk = self.request.recv(1024)
     data = str()
     data += chunk
@@ -144,27 +206,71 @@ class SyncHandler(SocketServer.BaseRequestHandler):
       chunk = self.request.recv(1024)
       data += chunk
 
+    return data
+
+  def handle(self):
+    logger.debug('Incoming request.')
+
+    data = self.read_incoming_request()
+
     with open(custom_file_path, 'w') as f:
       pass 
 
     parse(self.server.parser_, self.server.task_context_)
-
+    
     with open(custom_file_path, 'a') as f:
       f.write('\n' + data)
       f.flush()
 
-    print data
     sys.stdout.flush()
 
     parse(self.server.parser_, self.server.task_context_)
-    print '=================='
     sys.stdout.flush()
+
+    logger.debug('Parsing finished.')
+
+    reply = self.read_all_stream(self.server.read_stream_)
+    self.request.sendall(reply)
+
+  def read_all_stream(self, strm):
+    # pollster = select.poll()
+    # flags = select.POLLIN
+    # pollster.register(strm.fileno(), flags)
+
+    result = str()
+    # line = strm.read(100)
+    # result = line
+    # try:
+    #   while pollster.poll(0)[1]:
+    #     line = strm.read(1024)
+    #     if line:
+    #       result += line
+    #     else:
+    #       break
+    # except:
+    #   pass
+
+    max_read = 1024
+    while True:
+      chunk = os.read(strm.fileno(), max_read)
+      result += chunk
+      if len(chunk) < max_read:
+        break
+
+    return result
+
+
+def configure_stdout():
+  read_stream, write_stream = os.pipe()
+  os.dup2(write_stream, sys.stdout.fileno())
+  return os.fdopen(read_stream)
 
 
 def main(sess):
   sync_server = SocketServer.TCPServer(('0.0.0.0', 9999), SyncHandler)
   sync_server.task_context_ = RewriteContext(task_context_file)
   sync_server.parser_ = init(sess, sync_server.task_context_)
+  sync_server.read_stream_ = configure_stdout()
   sync_server.serve_forever()  
 
 
